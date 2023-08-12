638432246b886bcd57b2ab2ac3dc5a110385a01a
Victor Spirandeli <vspirandeli@gmail.com>
2023-08-12 20:48:28 -0300

    feat(front): initial commit. Configurações iniciais. Adicionado lint, auto-import, prettier e configuração padrão para tema escuro

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+// FIXME: FIX ME PLS
+export default function Home() {
+  return (
+    <main className="bg-white dark:bg-slate-700">
+      <h1 className="text-slate-950 dark:text-slate-500">
+        Wellcome To Casa da criança
+      </h1>
+    </main>
+  );
+}
