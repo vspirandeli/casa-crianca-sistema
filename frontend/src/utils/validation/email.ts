c04670e9b2efe76e44ff713f68a76b34997a7659
Victor Spirandeli <vspirandeli@gmail.com>
2023-08-14 20:17:03 -0300

    feat(validation): criado arquivo de validação de e-mails

---
+++ frontend/src/utils/validation/email.ts
@@ -0,0 +1,9 @@
+export function emailValidation(email: string): boolean {
+  const result = email
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    );
+
+  return Boolean(result);
+}
